2c8a329b42ab9773295c8051e8ca6a72103c7339
isa014 <alunoefdr.b29@futebolderua.org>
2024-09-03 10:23:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var personagem, torre;
+var Gemeover, gemeoverimg;
+function setup(){
+ createCanvas(600,600);
+
+ //criar os objetos do jogo
+ torre = createSprite(300,20,300,1200);
+ torre.shapeColor = 'black';
+ personagem = createSprite(200,200,10,10);
+ personagem.shapeColor = 'blue';
+
+}
+
+function draw(){
+ background('gray');
+
+  //movimento do personagem
+  if(keyDown('space')){
+   personagem.velocityY = -5;
+  }
+
+  //simula a gravidade
+  personagem.velocityY = personagem.velocityY + 0.8;
+
+  //movimento da torre
+  torre.velocityY = 1;
+
+  //torre não sair da tela
+  if(torre.y > 400){
+    torre.y = 400;
+  }
+  
+  //criar obstaculos 
+  gerarObstaculos();
+
+  drawSprites();
+
+}
+
+//criar a função de obstaculos
+
+function gerarObstaculos(){
+  //aqui iremos adicionar os comandos para criar todos os obstaculos
+  if(frameCount % 100 == 0){
+    var obstaculo = createSprite(200,10);
+    //vai gerar o obstaculo na posição aleatória entre 300 e 600
+    obstaculo.x = Math.round(random(300, 600));
+    obstaculo.y = Math.round(random(0, 800));
+    
+    //tempo de vida do obstaculo
+    obstaculo.lifetime = 200;
+     //funçao para funcionar o final do jogo
+    function draw(){
+      background(0);
+      if (gameState === "play") {
+        if(keyDown("left_arrow")){
+          gemeover.x = gemeover.x - 3;
+        }
+      }  
+     
